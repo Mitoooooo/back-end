507657cec4167bc47b910c666ad013217aab4579
Mitoooooo <duongxuanquangtran01@gmail.com>
2023-11-13 12:23:44 +0700

    Fix appdb

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -14,6 +14,11 @@ namespace Infrastructure
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<SportType> SportTypes { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<FieldCluster> FieldClusters { get; set; }
+        public DbSet<SportField> SportFields { get; set; }
+        public DbSet<SportFieldType> SportFieldType { get; set; }
+        public DbSet<SportFieldTypeSchedule> SportFieldTypeSchedules { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
